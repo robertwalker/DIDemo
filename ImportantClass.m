b1cd759b7ec1d2d08aafa879c6b37f84ebfe09ae
Robert Walker <robert4723@me.com>
2011-02-17 23:35:33 -0500

    Upgraded project for Xcode 4 and fix a warning.

--- ImportantClass.m
+++ ImportantClass.m
@@ -13,9 +13,11 @@
 
 - (id)initWithFoo:(NSObject <Fooing>*)aFoo
 {
-    if ( self = [super init] ) {
+    self = [super init];
+    if (self) {
         foo = [aFoo retain];
     }
+
     return self;
 }
 
